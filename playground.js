24c96ed1b2c945d7f69fddf262457a162ac903e2
NavasardianMichael <navasardianmichael@gmail.com>
2023-09-06 11:28:44 +0400

    1.2.1: new feaures added, fixed bugs

--- playground.js
+++ playground.js
@@ -2,13 +2,12 @@ const package = require('./dist')
 const list = package.genRecurDateBasedList({
   start: '2023-09-06T16:30:00',
   interval: 1,
-  intervalType: 'day',
-  numericTimezone: 4,
+  intervalType: 'hour',
+  numericTimezone: 6,
   direction: 'backward',
   end: 5,
-  exclude: ({ date }) => date < new Date(),
+  // exclude: ({ date }) => date > new Date(),
   extend: {
-    isMonday: ({ date }) => date.getDay() === 1,
     timeStr: ({ dateStr }) => dateStr.split('T')[1]
   },
   onError: (error) => {
